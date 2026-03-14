82a83f8935856ce164f22d038f146bf462efc067
Alin V. (Claudiu) <alin.voinea@gmail.com>
2026-03-14 09:24:47 +0200

    tests: use search approach for block chooser in Volto 18 cypress test

--- cypress/e2e/01-block-basics.cy.js
+++ cypress/e2e/01-block-basics.cy.js
@@ -17,8 +17,10 @@ describe('Blocks Tests', () => {
 
     // Add block
     cy.get('.ui.basic.icon.button.block-add-button').first().click();
-    cy.get('.blocks-chooser .title').contains('Media').click();
-    cy.get('.content.active.media .button.image').contains('Image').click();
+    cy.get(".blocks-chooser .ui.form .field.searchbox input[type='text']").type(
+      'Image',
+    );
+    cy.get('.button.image').contains('Image').click({ force: true });
 
     // Save
     cy.get('#toolbar-save').click();
